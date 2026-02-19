e89f20944cac6d321908c0e3a8ffc4d66ce2ad37
Matthew Downs <22185242+mwdowns@users.noreply.github.com>
2026-02-19 18:30:45 -0500

    compile ts

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1.default.get(url).then(function (response) {
-    console.log(response.data);
+    var data = response.data;
+    var id = data.id;
+    var title = data.title;
+    var completed = data.completed;
+    logTodo(id, title, completed);
 });
+var logTodo = function (id, title, completed) {
+    console.log("\n    The Todo with ID: ".concat(id, "\n    Has a title of ").concat(title, "\n    And is ").concat(completed ? "finished" : "not finished", "\n  "));
+};
